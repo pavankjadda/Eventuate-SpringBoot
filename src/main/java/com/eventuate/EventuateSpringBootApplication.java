5c0c0e86cff7a51281c4b0edaf443127d4216c29
pjadda <jpavanaryan@gmail.com>
2018-04-02 16:18:49 -0400

    First Commit

---
+++ src/main/java/com/eventuate/EventuateSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.eventuate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventuateSpringBootApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EventuateSpringBootApplication.class, args);
+	}
+}
